7b9026e030e508704815bdc04fedaa6e7276917c
lmaaaad <imad.boukedjani@gmail.com>
2022-06-06 23:10:16 +0100

    final

--- resources/views/rapport.blade.php
+++ resources/views/rapport.blade.php
@@ -250,25 +250,7 @@
         <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
@@ -322,7 +304,7 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: [ 'Jan','Fev','Mars','Avril','MAi','Juin','Juill','Aout','Sept','Oct','Nov','Dec'],
+    labels:{{  json_encode(array_keys($depense)) }},
     datasets: [{
       label: "Depenses  ",
       lineTension: 0.5,
