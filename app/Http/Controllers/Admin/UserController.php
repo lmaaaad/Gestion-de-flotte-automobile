92cfc1d95a6a59f033de7d8fa43636d52a2ea2a3
boukedjani imad <imad.boukedjai@gmail.com>
2024-02-14 20:27:59 +0100

    final-push

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -118,7 +118,7 @@ class UserController extends Controller
         $user = User::find($id); 
         if(!$user)
         {
-            $request->session()->flash('error','Utilisateur Introuvable !!');
+            $request->session()->flash('error','Utilisateur Introuvable !!!');
             return redirect(route('admin.users.index')); 
 
         }
